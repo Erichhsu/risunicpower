85679772ca8cb0b2876723df1ca4d6c51ef05c4b
RisunicPower Dev <dev@risunicpower.com>
2026-06-29 15:26:20 +0800

    fix: restore createNextIntlPlugin to bundle next-intl request config in production

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
 import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin('./src/lib/i18n/request.ts')
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.111.144', '172.19.224.1'],
@@ -12,4 +15,4 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['nodemailer'],
 }
 
-export default nextConfig
+export default withNextIntl(nextConfig)
